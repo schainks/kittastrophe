632a418dd409aca5572e593eb5214eb54b194a06
David Schainker <david@schainker.net>
2018-06-11 20:34:29 +0800

    stubbing first web request WIP

--- lib/kittastrophe/client.rb
+++ lib/kittastrophe/client.rb
@@ -3,8 +3,22 @@ require 'optparse'
 
 module Kittastrophe
   class Client
-    def hi()
-      puts "Schleeb"
+    api_base_uri "https://typekit.com/api/v1/json"
+
+    def zoidberg()
+      "Hooray, I'm useful!"
     end
+
+    def list()
+      #key = read_api_key
+      #http = Curl::Easy.perform(api_base_uri + '/list') do |curl|
+      #  curl.headers["X-Typekit-Token"] = "token"
+      #  # don't forget to use on_missing, on_failure, on_success
+      #end
+      #http.perform
+      #http.body_str
+      "haha"
+    end
+
   end
 end
--- spec/kittastrophe_spec.rb
+++ spec/kittastrophe_spec.rb
@@ -1,10 +1,35 @@
 require 'kittastrophe/client'
 
 require 'curb'
+
 require 'optparse'
+require 'json'
 
 RSpec.describe Kittastrophe::Client do
-  it "says hi" do
-    expect(Kittastrophe::Client.hi()).to eql("Schleeb")
+  ## sample /list output
+  #    {
+  #       "kits" : [
+  #          {
+  #             "id" : "inp1avm",
+  #             "link" : "/api/v1/json/kits/inp1avm"
+  #          }
+  #       ]
+  #    }
+  let(:list_response) { {"kits" => [{ "id" => "foobars", "link" => "/api/v1/json/kits/foobars"}]} }
+
+  it "has a wonderful time" do
+    expect(Kittastrophe::Client.new.zoidberg()).to eql("Hooray, I'm useful!")
   end
+
+  it "lists available kits" do
+    stub = stub_request(:get, "https://typekit.com").
+      with(headers: {'X-Typekit-Token': "blah"}).
+      to_return(:status => [200, response.to_json])
+    expect(Kittastrophe::Client.new.list()).to eql("haha")
+    expect(stub).to have_been_requested
+  end
+
+  #test for config
+  # test for list
+  # test for show
 end
