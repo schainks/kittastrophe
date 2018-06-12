a3660de92e13cb39dd117821c3b335b44b87818f
David Schainker <david@schainker.net>
2018-06-12 10:16:37 -0700

    fixed up some of the tests with mocked API responses taken from real world, can safely continue with test suite

--- lib/kittastrophe/client.rb
+++ lib/kittastrophe/client.rb
@@ -1,24 +1,79 @@
 require 'curb'
 require 'optparse'
+require 'json'
 
 module Kittastrophe
   class Client
-    api_base_uri "https://typekit.com/api/v1/json"
 
-    def zoidberg()
+    @@KEY_PATH = File.join(Dir.home, ".typekit")
+
+    def initialize()
+      @api_key = read_api_key
+    end
+
+    def key_path
+      @@KEY_PATH
+    end
+
+    def zoidberg
       "Hooray, I'm useful!"
     end
 
-    def list()
-      #key = read_api_key
-      #http = Curl::Easy.perform(api_base_uri + '/list') do |curl|
-      #  curl.headers["X-Typekit-Token"] = "token"
-      #  # don't forget to use on_missing, on_failure, on_success
-      #end
-      #http.perform
-      #http.body_str
-      "haha"
+    def list
+      http = Curl.get("https://typekit.com/api/v1/json/kits") do |http|
+        http.headers['X-Typekit-Token'] = @api_key
+      end
+
+      body = JSON.parse(http.body_str)
+
+      if http.response_code != 200
+        process_error(body)
+      else
+        body
+      end
     end
 
+    def show(id)
+      http = Curl.get("https://typekit.com/api/v1/json/kits/#{id}") do |http|
+        http.headers['X-Typekit-Token'] = @api_key
+      end
+
+      body = JSON.parse(http.body_str)
+      if http.response_code != 200
+        process_error(body)
+      else
+        body
+      end
+    end
+
+    def process_error(body)
+      body[:errors]
+    end
+
+    def configure
+      api_key = prompt "Please enter your TypeKit API key: "
+
+      open(@@KEY_PATH, 'w') do |f|
+        f.puts "#{api_key}".strip
+        f.close
+      end
+      exit
+    end
+
+    def prompt(*args)
+        print(*args)
+        gets
+    end
+
+    def read_api_key
+      if File.exists?(@@KEY_PATH)
+        file = File.open(@@KEY_PATH, "rb")
+        contents = file.read
+        file.close
+        key = contents.strip
+      end
+
+      ENV['TYPEKIT_API_KEY'] || key
+    end
   end
 end
--- spec/kittastrophe_spec.rb
+++ spec/kittastrophe_spec.rb
@@ -6,30 +6,75 @@ require 'optparse'
 require 'json'
 
 RSpec.describe Kittastrophe::Client do
-  ## sample /list output
-  #    {
-  #       "kits" : [
-  #          {
-  #             "id" : "inp1avm",
-  #             "link" : "/api/v1/json/kits/inp1avm"
-  #          }
-  #       ]
-  #    }
-  let(:list_response) { {"kits" => [{ "id" => "foobars", "link" => "/api/v1/json/kits/foobars"}]} }
+  let(:token) { "token" }
+  let(:list_response) { {"kits"=>[{"id"=>"inp1avm", "link"=>"/api/v1/json/kits/inp1avm"}]} }
+  let(:error_response) { '''{
+                               "errors" : [
+                                  "Not Found"
+                               ]
+                            }''' }
+
+  let(:list_empty_response) { {"kits" => []} }
+
 
   it "has a wonderful time" do
-    expect(Kittastrophe::Client.new.zoidberg()).to eql("Hooray, I'm useful!")
+    expect(subject.zoidberg()).to eql("Hooray, I'm useful!")
   end
 
   it "lists available kits" do
-    stub = stub_request(:get, "https://typekit.com").
-      with(headers: {'X-Typekit-Token': "blah"}).
-      to_return(:status => [200, response.to_json])
-    expect(Kittastrophe::Client.new.list()).to eql("haha")
+    subject.instance_variable_set(:@api_key,  token)
+
+    stub = stub_request(:get, "https://typekit.com/api/v1/json/kits").
+      with(headers: {'X-Typekit-Token': token }).
+      to_return(:status => 200, body: list_response.to_json)
+    expect(subject.list).to eql(list_response)
     expect(stub).to have_been_requested
   end
 
-  #test for config
-  # test for list
-  # test for show
+  it "lists empty kit list" do
+    subject.instance_variable_set(:@api_key, token)
+
+    stub = stub_request(:get, "https://typekit.com/api/v1/json/kits").
+      with(headers: {'X-Typekit-Token': token }).
+      to_return(:status => 200, :body => list_empty_response.to_json)
+    expect(subject.list).to eql(list_empty_response)
+    expect(stub).to have_been_requested
+  end
+
+  it "shows a specific kits by ID" do
+
+  end
+
+  it "shows that you asked for a kit that isn't there" do
+
+  end
+
+  it "shows you are unauthorized" do
+
+  end
+
+  it "loads api key from file" do
+    # make key file with stubbed key
+    # unset the ENV Var
+    ENV.delete('TYPEKIT_API_KEY')
+
+    open(subject.key_path, 'w') do |f|
+      f.puts "#{token}".strip
+      f.close
+    end
+    expect(subject.read_api_key).to eql(token)
+  end
+
+  it "loads api key from env var" do
+
+  end
+
+  it "makes sure api key in ENV var has precedent over loaded file" do
+
+  end
+
+  it "configures an api key" do
+
+  end
+
 end
